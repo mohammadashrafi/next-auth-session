a270dac967d513556d11960d7a29c23f593f1d5a
mohammad ashrafi <mohammadashrafi7@gmail.com>
2024-05-26 14:32:28 +0330

    fix: signout

--- src/app/auth/signout/page.js
+++ src/app/auth/signout/page.js
@@ -31,10 +31,7 @@ export default function SignOutButton() {
                         <button
                             className="py-2.5 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm"
                             type="button"
-                            onClick={() =>{
-                            signOut({ callbackUrl: "/" })
-                               signoutfunc()
-                            } }
+                            onClick={() =>signOut({ callbackUrl: "/" }) }
                         >
                             آره مطمئنم
                         </button>
