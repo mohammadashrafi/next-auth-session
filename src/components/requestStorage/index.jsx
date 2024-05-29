32a89c5d2fa814a3a677cce4b24b493091ad3878
mohammad ashrafi <mohammadashrafi7@gmail.com>
2024-05-29 15:51:31 +0330

    new role access to storage

--- src/components/requestStorage/index.jsx
+++ src/components/requestStorage/index.jsx
@@ -1,20 +1,47 @@
 "use client"
 import React from 'react'
 
-const RequestStorage = () => {
+const RequestStorage =() => {
 
-const handelClick=()=>{
-document.hasStorageAccess().then((hasaccess)=>{
-    if(!hasaccess){
-        return document.requestStorageAccess()
+const handelClick= async()=>{
+
+//   // Check if Storage Access API is supported
+//   if (!document.requestStorageAccess) {
+//     // Storage Access API is not supported so best we can do is
+//     // hope it's an older browser that doesn't block 3P cookies
+//     console.log("Storage Acccess API not supported. Assume we have access.")
+//     return true;
+//   }
+
+//   // Check if access has already been granted
+//   if (await document.hasStorageAccess()) {
+//     console.log("Cookie access already granted");
+//     return true;
+//   }
+
+navigator.permissions.query({ name: "storage-access" }).then(async (result) => {
+    if (result.state === "granted") {
+        try {
+            console.log("Cookie access allowed. Calling requestStorageAccess()");
+            await document.requestStorageAccess();
+            console.log("Cookie access granted");
+            return true;
+          } catch (error) {
+            // This shouldn't really fail if access is granted
+            return false;
+          }
+     
+    } else if (result.state === "prompt") {
+        console.log("Cookie access requires a prompt");
+        return false
+   
+    }else if (result.state ==="denied"){
+        console.log("Cookie access denied");
+        return false;
     }
+    // Don't do anything if the permission was denied.
+  });
 
-}).then(()=>{
-    console.log("now we have access to the cookie")
-    console.log(`cookie :${document.cookie}`)
-}).catch((error)=>{
-    console.log(error)
-})
 
 
 }
