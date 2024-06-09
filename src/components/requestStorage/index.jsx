a687c7420f45e182960139646170c91edc643a9f
mohammad ashrafi <mohammadashrafi7@gmail.com>
2024-06-09 18:34:16 +0330

    push access cookie

--- src/components/requestStorage/index.jsx
+++ src/components/requestStorage/index.jsx
@@ -4,27 +4,98 @@ const RequestStorage =() => {
 
 
 
-
+  function doThingsWithCookies() {
+    document.cookie = "mammmmmmad=1234"; // set a cookie
+    console.log("document.cookie",document.cookie)
+  }
+  
+  function doThingsWithLocalStorage(handle) {
+    handle.localStorage.setItem("foo", "bar"); // set a local storage key
+  }
+  
+  async function handleCookieAccess() {
+    if (!document.hasStorageAccess) {
+      // This browser doesn't support the Storage Access API
+      // so let's just hope we have access!
+      doThingsWithCookies();
+    } else {
+      const hasAccess = await document.hasStorageAccess();
+      if (hasAccess) {
+        // We have access to third-party cookies, so let's go
+        doThingsWithCookies();
+        // If we want to modify unpartitioned state, we need to request a handle.
+        const handle = await document.requestStorageAccess({
+          localStorage: true,
+        });
+        doThingsWithLocalStorage(handle);
+      } else {
+        // Check whether third-party cookie access has been granted
+        // to another same-site embed
+        try {
+          const permission = await navigator.permissions.query({
+            name: "storage-access",
+          });
+  
+          if (permission.state === "granted") {
+            // If so, you can just call requestStorageAccess() without a user interaction,
+            // and it will resolve automatically.
+            const handle = await document.requestStorageAccess({
+              cookies: true,
+              localStorage: true,
+            });
+            doThingsWithLocalStorage(handle);
+            doThingsWithCookies();
+          } else if (permission.state === "prompt") {
+            // Need to call requestStorageAccess() after a user interaction
+           
+              try {
+                const handle = await document.requestStorageAccess({
+                  cookies: true,
+                  localStorage: true,
+                });
+                doThingsWithLocalStorage(handle);
+                doThingsWithCookies();
+              } catch (err) {
+                // If there is an error obtaining storage access.
+                console.error(`Error obtaining storage access: ${err}.
+                              Please sign in.`);
+              }
+          
+          } else if (permission.state === "denied") {
+            // User has denied third-party cookie access, so we'll
+            // need to do something else
+          }
+        } catch (error) {
+          console.log(`Could not access permission state. Error: ${error}`);
+          doThingsWithCookies(); // Again, we'll have to hope we have access!
+        }
+      }
+    }
+  }
+  
     
 
 
 
 
- const handelClick= async()=>{
+//  const handelClick= async()=>{
 
 
-  document.hasStorageAccess().then(hasAccess => {
-    console.log('hasAccess: ' + hasAccess);
-    if (!hasAccess) {
-      return document.requestStorageAccess();
-    }
-  }).then(_ => {
-    console.log('Now we have first-party storage access!');
-    document.cookie = "foo=bar";
-    console.log(`document.cookie: ${document.cookie}`);
-  }).catch(_ => {
-    console.log('error');
-  });
+//   document.hasStorageAccess().then(hasAccess => {
+//     console.log('hasAccess: ' + hasAccess);
+//     if (!hasAccess) {
+//       return document.requestStorageAccess({
+//         cookies: true,
+//         localStorage: true
+//       });
+//     }
+//   }).then(_ => {
+//     console.log('Now we have first-party storage access!');
+//     document.cookie = "foo=bar";
+//     console.log(`document.cookie: ${document.cookie}`);
+//   }).catch(_ => {
+//     console.log('error');
+//   });
 
 
 // console.log("start")
@@ -129,7 +200,7 @@ const RequestStorage =() => {
 //     console.error('Error checking storage permission:', error);
 //   });
 
-}
+//}
 
 const handelaccessstorage=() => {
   
@@ -147,11 +218,66 @@ console.log("parentCookies",parentCookies);
 }
 
 
+function rSAFor() {
+  if ("requestStorageAccessFor" in document) {
+    document.requestStorageAccessFor("https://storageaccess.netlify.app").then(
+      (res) => {
+        doThingsWithCookies();
+      },
+      (err) => {
+        // Handle errors
+      },
+    );
+  }
+}
+
+
+
+
+const secondaccess=()=>{
+  navigator.permissions
+  .query({
+    name: "top-level-storage-access",
+    requestedOrigin: "https://storageaccess.netlify.app/",
+  })
+  .then((permission) => {
+    if (permission.state === "granted") {
+      // Permission has already been granted
+      // No need to call requestStorageAccessFor() again, just start using cookies
+      doThingsWithCookies();
+    } else if (permission.state === "prompt") {
+      // Need to call requestStorageAccessFor() after a user interaction
+    
+        // Request storage access
+        rSAFor();
+   
+    } else if (permission.state === "denied") {
+      // User has denied third-party cookie access, so we'll
+      // need to do something else
+    }
+  });
+}
+
+
+const thirdaccess=()=>{
+
+}
+
+
+
   return (
     <div>
-      <button className='m-auto bg-red-500 p-5' onClick={handelClick}>storage</button>
+      <button className='m-auto bg-red-500 p-5' >storage</button>
 <br />
-      <button className='m-auto bg-green-500 p-5' onClick={handelaccessstorage}>access localstorage</button>
+      <button className='m-auto bg-red-500 p-5 my-5'  onClick={handelaccessstorage}>access localstorage</button>
+
+
+      <button className='m-auto bg-red-500 p-5 my-5' onClick={handleCookieAccess}>access cookie 1</button>
+
+      <button className='m-auto bg-red-500 p-5 my-5' onClick={secondaccess}>access cookie 2</button>
+
+
+      
     </div>
   )
 }
